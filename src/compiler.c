ff008d3122f149c3acc5c0c0f41e79b0de4adeb8
CleanIce <CleanIce2023@outlook.com>
2025-12-02 22:46:35 +0800

    Update 2025/12/2

--- src/compiler.c
+++ src/compiler.c
@@ -4,22 +4,55 @@
 #include "compiler.h"
 #include "scanner.h"
 
-void compile(const char* source) {
-    init_scanner(source);
+typedef struct {
+    Token current;
+    Token previous;
+    bool had_error;
+} Parser;
 
-    int32 line = -1;
+Parser parser;
+
+static error_at(Token* token, const char* message) {
+    fprintf(stderr, "[line %d] Error", token->line);
+
+    if (token->type == TokenEOF) {
+        fprintf(stderr, " at end");
+    } else if (token->type == TokenError) {
+        // empty.
+    } else {
+        fprintf(stderr, " at `%.*s`", token->length, token->start);
+    }
+
+    fprintf(stderr, ": %s\n", message);
+    parser.had_error = true;
+}
+
+static void error(const char* message) {
+    error_at(&parser.previous, message);
+}
+
+static void error_at_current(const char* message) {
+    error_at(&parser.current, message);
+}
+
+static void advance() {
+    parser.previous = parser.current;
+
     while (true) {
-        Token token = scan_token();
-        if (token.line != line) {
-            printf("%4d ", token.line);
-            line = token.line;
-        } else {
-            printf("   | ");
-        }
-        printf("%2d `%.*s`\n", token.type, token.length, token.start);
-
-        if (token.type == TokenEOF) {
+        parser.current = scan_token();
+        if (parser.current.type != TokenError) {
             break;
         }
+        error_at_current(parser.current.start);
     }
+}
+
+bool compile(const char* source, Chunk* chunk) {
+    init_scanner(source);
+
+    advance();
+    expression();
+    consume(TokenEOF, "Expect end of expression.");
+
+    return !parser.had_error;
 }
