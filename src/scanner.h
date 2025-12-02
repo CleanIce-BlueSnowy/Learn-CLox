9f96c2c800d6763cfa23e4751589e6eac53e4fed
CleanIce <CleanIce2023@outlook.com>
2025-12-02 22:08:21 +0800

    Update 2025/12/2

--- src/scanner.h
+++ src/scanner.h
@@ -1,3 +1,34 @@
 #pragma once
 
+#include "common.h"
+
+typedef enum {
+    TokenLeftParen, TokenRightParen,
+    TokenLeftBrace, TokenRightBrace,
+    TokenComma, TokenDot, TokenMinus, TokenPlus,
+    TokenSemicolon, TokenSlash, TokenStar,
+
+    TokenBang, TokenBangEqual,
+    TokenEqual, TokenEqualEqual,
+    TokenGreater, TokenGreaterEqual,
+    TokenLess, TokenLessEqual,
+
+    TokenIdentifier, TokenString, TokenNumber,
+
+    TokenAnd, TokenClass, TokenElse, TokenFalse,
+    TokenFor, TokenFun, TokenIf, TokenNil,
+    TokenOr, TokenPrint, TokenReturn, TokenSuper,
+    TokenThis, TokenTrue, TokenVar, TokenWhile,
+
+    TokenError, TokenEOF,
+} TokenType;
+
+typedef struct {
+    TokenType type;
+    const char* start;
+    int32 length;
+    int32 line;
+} Token;
+
 void init_scanner(const char* source);
+Token scan_token();
